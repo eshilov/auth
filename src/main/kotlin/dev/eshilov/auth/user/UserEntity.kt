9a894cb8a6e9ef68c6ff894311f530ea1ce1e57f
Evgeny Shilov <60179259+eshilov@users.noreply.github.com>
2022-05-08 20:42:22 +0500

    Create basic user entity

---
+++ src/main/kotlin/dev/eshilov/auth/user/UserEntity.kt
@@ -0,0 +1,26 @@
+package dev.eshilov.auth.user
+
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "users")
+class UserEntity(
+
+    @Id
+    @GeneratedValue
+    var id: UUID? = null,
+
+    @Column(name = "first_name")
+    var firstName: String,
+
+    @Column(name = "last_name")
+    var lastName: String,
+
+    @Column(name = "password_hash")
+    var passwordHash: String
+)
